48845bd27ffb61682c2c8b8eba6b85c519c4a32a
sstardellis <sjstardellis@gmail.com>
2025-08-14 19:57:39 -0400

    cleaner dayOfYear solution

--- Math/dayOfYear.py
+++ Math/dayOfYear.py
@@ -1,37 +1,17 @@
 class Solution:
     def dayOfYear(self, date: str) -> int:
-        # slice out the string
+        # slice the numbers out
         year = int(date[:4])
-        input_month = int(date[5:7])
+        month = int(date[5:7])
         day = int(date[8:])
 
-        # set February days to 28, check if a leap year
-        feb_days = 28
+        # days per month (Feb = 28)
+        month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+        # leap year check
         if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-            feb_days = 29
+            # set February to 29 if true
+            month_days[1] = 29
 
-        # our dictionary to store our months and corresponding days
-        month_days = {
-            1: 31,
-            2: feb_days,
-            3: 31,
-            4: 30,
-            5: 31,
-            6: 30,
-            7: 31,
-            8: 31,
-            9: 30,
-            10: 31,
-            11: 30,
-            12: 31
-        }
-
-        # sum all the days before the inputted month
-        total_days_before = sum(
-            # days at the beginning is the filter for the tuple
-            # .month, days in month_days.items() returns each key–value pair from the dictionary
-            # filter condition: if int(month) < int(input_month)
-            days for month, days in month_days.items() if int(month) < int(input_month)
-        )
-
-        return total_days_before + day
+        # sum days of all previous months + current day
+        return sum(month_days[:month-1]) + day
